29e2919fb5a6b59f68b4c3a01dd71f9d226fb9e8
Szakony Peter <peter.szakony@hotmail.com>
2025-08-13 16:00:36 +0200

    fix get best study wrong path

--- get_best_study.py
+++ get_best_study.py
@@ -1,6 +1,6 @@
 import optuna
-storage_name = "sqlite:///my_study.db"
-study_name = "transformer-optimization-v1"  # Give your study a name
+storage_name = "sqlite:///data/my_study.db"
+study_name = "encoder-optimization-v1"  # Give your study a name
 loaded_study = optuna.load_study(study_name=study_name, storage=storage_name)
 print(loaded_study.best_params)
 
